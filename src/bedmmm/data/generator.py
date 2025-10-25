713de3434be7c548053df410d24d927fdbab3cb5
triluu-sellforte <tri.luu@sellforte.com>
2025-10-25 16:47:11 +0300

    Finalize the data generator

--- src/bedmmm/data/generator.py
+++ src/bedmmm/data/generator.py
@@ -1,27 +1,46 @@
 """Module to generate synthetic data."""
 
+import logging
 from typing import Literal, Self
 
-from numpy.typing import NDArray
+import numpy as np
+import polars as pl
+from bedmmm.functions.carryover import adstock_numpy
+from bedmmm.functions.saturation import exp_numpy
+
+logger = logging.getLogger(__name__)
 
 
 class DataGenerator:
     """Synethetic data generator."""
 
-    def __init__(self: Self, n_time_periods: int):
-        """Initialization.
+    def __init__(self: Self, n_time_periods: int, random_seed: int = 0):
+        """Initialize.
 
         Parameters
         ----------
         n_time_periods : int
             The number of weeks for the data generation.
+        random_seed : int, default 0
+            The seed for the random generator.
+
 
         """
+        np.random.seed(random_seed)
         self.T = n_time_periods
-        self._baseline: NDArray | None = None
-        self._control: NDArray | None = None
-        self._media: NDArray | None = None
-        self._target: NDArray | None = None
+
+        self.M = 0  # Number of media channels generated
+        self.C = 0  # number of control variables generated
+
+        self._baseline_df: pl.DataFrame = pl.DataFrame()
+
+        self._control_df: pl.DataFrame = pl.DataFrame()
+        self._control_params = []
+
+        self._media_df: pl.DataFrame = pl.DataFrame()
+        self._media_params = []
+
+        self._target_df: pl.DataFrame = pl.DataFrame()
 
     def generate_baseline(self: Self, base: float, noise: float) -> Self:
         """Generate baseline.
@@ -40,22 +59,111 @@ class DataGenerator:
             DataGenerator with baseline included.
 
         """
+        baseline = base + np.random.normal(loc=0, scale=noise, size=self.T)
+        self._baseline_df = self._baseline_df.with_columns(
+            pl.Series("baseline", baseline)
+        )
         return self
 
-    def generate_control(self: Self) -> Self:
-        """Generate the effects of one control variable."""
+    def generate_control(
+        self: Self,
+        control_effect: float,
+        noise: float,
+        type: Literal["seasonality"] = "seasonality",
+    ) -> Self:
+        """Generate the arbitrary control variable.
+
+        Parameters
+        ----------
+        control_effect : float
+            The effect coefficient of the control variable.
+        noise : float
+            The noise added to the control variable.
+        type : str, default "seasonality"
+            Type of control variable to generate. Supported types are:
+                - seasonality
+
+        """
+        self.C += 1
+        match type:
+            case "seasonality":
+                control_var = np.cos(
+                    2 * np.pi * (np.arange(self.T) - 12) / 52
+                ) + np.random.normal(loc=0, scale=noise, size=self.T)
+
+                self._control_df = self._control_df.with_columns(
+                    pl.Series(f"control_{self.C}", control_var)
+                )
+                self._control_params.append(
+                    {
+                        "control_variable": f"control_{self.C}",
+                        "control_effect": control_effect,
+                    }
+                )
         return self
 
-    def generate_media(self: Self) -> Self:
-        """Generate the metrics of one media channel."""
+    def generate_media(
+        self: Self,
+        retention_rate: float,
+        saturation: float,
+        shape: float,
+        noise: float,
+    ) -> Self:
+        """Generate the metrics of one media channel.
+
+        Parameters
+        ----------
+        retention_rate : float, between 0 and 1
+            The retention rate of the channel.
+            Used in adstock function for carryover effects.
+        saturation : float
+            The saturation level of the channel.
+            Used in exponential saturation function.
+        shape : float
+            The shape of the channel.
+            Used in exponential saturation function.
+        noise : float
+            The noise added to the media metrics.
+
+        """
+        if self.C == 0:
+            raise ValueError(
+                "There is no control variable generated yet! Please generate "
+                "at least one control variable before any media data."
+            )
+
+        self.M += 1
+
+        metric_lb = np.random.uniform(0, 1)
+        control_corr = np.random.uniform(0, 1)
+        white_noise = np.random.normal(0, noise, self.T)
+
+        media_metrics = (
+            metric_lb
+            + control_corr * self._control_df.to_numpy().sum(axis=1)
+            + np.sqrt(1 - control_corr**2) * white_noise
+        )
+
+        self._media_df = self._media_df.with_columns(
+            pl.Series(f"media_{self.M}", media_metrics)
+        )
+        self._media_params.append(
+            {
+                "media_feature": f"media_{self.M}",
+                "retention_rate": retention_rate,
+                "saturation": saturation,
+                "shape": shape,
+            }
+        )
+
         return self
 
-    def generate_target(self: Self, noise_sigma: float) -> Self:
+    def generate_target(self: Self, noise: float) -> Self:
         """Generate the sales target with noise included.
 
         Parameters
         ----------
-        noise_sigma : float
+        noise : float
             The observation noise added to the sales target.
             The noise is generated based on the normal distribution
             with mean zero.
@@ -66,13 +174,48 @@ class DataGenerator:
             DataGenerator with target included.
 
         """
+        if not self._target_df.is_empty():
+            logger.warning(
+                "A set of target data has already generated before. "
+                "Overwriting it!"
+            )
+
+        media_uplifts = [
+            media_param["saturation"]
+            * exp_numpy(
+                x=adstock_numpy(
+                    x=self._media_df[media_param["media_feature"]].to_numpy(),
+                    retention_rate=media_param["retention_rate"],
+                ),
+                shape=media_param["shape"],
+            )
+            for media_param in self._media_params
+        ]
+
+        control_effects = [
+            control_param["control_effect"]
+            * self._control_df[control_param["control_variable"]].to_numpy()
+            for control_param in self._control_params
+        ]
+
+        white_noise = np.random.normal(0, noise, self.T)
+
+        y_target = (
+            np.sum(media_uplifts, axis=0)
+            + np.sum(control_effects, axis=0)
+            + self._baseline_df["baseline"].to_numpy()
+            + white_noise
+        )
+
+        self._target_df = self._target_df.with_columns(
+            pl.Series("y", y_target)
+        )
+
         return self
 
     def collect(
         self: Self,
-    ) -> dict[
-        Literal["baseline", "control", "media", "target"], NDArray | None
-    ]:
+    ) -> pl.DataFrame:
         """Collect the generated data.
 
         Returns
@@ -82,20 +225,36 @@ class DataGenerator:
             If None, the data has not been generated.
 
         """
-        return {
-            "baseline": self._baseline,
-            "control": self._control,
-            "media": self._media,
-            "target": self._target,
-        }
+        return pl.concat(
+            [
+                self._media_df,
+                self._control_df,
+                self._target_df,
+            ],
+            how="horizontal",
+        )
 
+    def get_parameters(self: Self) -> tuple[pl.DataFrame, pl.DataFrame]:
+        """Get the parameters used in the data generation.
 
-if __name__ == "__main__":
-    generator = DataGenerator(104)
-    generated_data = (
-        generator.generate_baseline(base=10, noise=0.5)
-        .generate_control()
-        .generate_control()
-        .generate_media()
-        .collect()
-    )
+        Returns
+        -------
+        tuple[pl.DataFrame, pl.DataFrame]
+            The media and control parameters as DataFrames.
+
+        """
+        return pl.DataFrame(
+            self._media_params,
+            schema={
+                "media_feature": pl.String,
+                "retention_rate": pl.Float64,
+                "saturation": pl.Float64,
+                "shape": pl.Float64,
+            },
+        ), pl.DataFrame(
+            self._control_params,
+            schema={
+                "control_variable": pl.String,
+                "control_effect": pl.Float64,
+            },
+        )
