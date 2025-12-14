da607f5e2e7556626f548a3ad9e4048f70ba75d9
triluu-sellforte <tri.luu@sellforte.com>
2025-12-14 15:39:55 +0200

    plot: update the syntax of posterior vs ground truth figures

--- src/thesis/plot/posterior_distribution.py
+++ src/thesis/plot/posterior_distribution.py
@@ -2,13 +2,15 @@
 
 import arviz as az
 import matplotlib.pyplot as plt
+import numpy as np
 import polars as pl
 
 
 def plot_posterior_vs_ground_truth(
     idata: az.InferenceData,
     media_param_df: pl.DataFrame,
-    media_feature_name: str,
+    media_feature_names: list[str],
+    figsize: tuple[int, int] = (18, 5),
 ) -> None:
     """Plot the posterior distributions along with the ground truth.
 
@@ -18,40 +20,77 @@ def plot_posterior_vs_ground_truth(
         The inference data containing posterior distributions.
     media_param_df : pl.DataFrame
         The media parameters dataframe.
-    media_feature_name : str
-        The media feature name to plot against.
+    media_feature_names : list[str]
+        The list of media feature names to plot against.
+    figsize : tuple[int, int], default (18, 5)
+        The size of the Matplotlib Figure.
 
     """
-    _, ax = plt.subplots(1, 3, figsize=(18, 5))
+    fig, ax = plt.subplots(
+        nrows=len(media_feature_names),
+        ncols=3,
+        figsize=figsize,
+    )
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.95, hspace=0.3)
 
-    media_feature_id = int(media_feature_name.split("_")[1])
+    if len(media_feature_names) == 1:
+        ax = ax[np.newaxis, :]
 
-    for i, param in enumerate(["saturation", "shape", "retention_rate"]):
-        ax[i].hist(
-            idata.posterior[f"{param}s"]
-            .values[:, :, media_feature_id]
-            .flatten(),
-            bins=100,
-            density=True,
-            histtype="step",
-            alpha=0.7,
-            label="posterior distributions",
+    media_feature_ids = [
+        int(name.split("_")[1]) for name in media_feature_names
+    ]
+
+    legend_handles, legend_labels = ([], [])
+    for row, (media_feature_id, media_feature_name) in enumerate(
+        zip(media_feature_ids, media_feature_names)
+    ):
+        for col, param in enumerate(["saturation", "shape", "retention_rate"]):
+            h1 = ax[row, col].hist(
+                idata.posterior[f"{param}s"]
+                .values[:, :, media_feature_id]
+                .flatten(),
+                bins=100,
+                density=True,
+                histtype="step",
+                alpha=0.7,
+                label="posterior distributions",
+            )
+            h2 = ax[row, col].vlines(
+                media_param_df.filter(
+                    pl.col("media_feature") == media_feature_name
+                )[param].item(),
+                ymin=0,
+                ymax=ax[row, col].get_ylim()[1],
+                colors="C2",
+                label="ground truth",
+            )
+            ax[row, col].set_title(f"{param.capitalize().replace('_', ' ')}")
+
+            if len(legend_handles) == 0:
+                legend_handles = [h1[2][0], h2]
+                legend_labels = ["Posterior distribution", "Ground truth"]
+
+        # Add the title of each row
+        row_axes = ax[row, :]
+        y_top = max(a.get_position().y1 for a in row_axes)
+        fig.text(
+            x=0.5,
+            y=y_top + 0.1 / len(media_feature_names),
+            s=(
+                f"Posterior distributions vs Ground Truth of "
+                f"Media Channel {media_feature_id + 1}"
+            ),
+            ha="center",
+            va="center",
+            fontsize=15,
         )
-        ax[i].vlines(
-            media_param_df.filter(
-                pl.col("media_feature") == media_feature_name
-            )[param].item(),
-            ymin=0,
-            ymax=ax[i].get_ylim()[1],
-            colors="C2",
-            label="ground truth",
-        )
-        ax[i].set_title(f"{param.capitalize().replace('_', ' ')}")
 
-    plt.legend()
-    plt.suptitle(
-        f"Posterior distributions vs Ground Truth of "
-        f"{media_feature_name.capitalize().replace('_', ' ')}"
+    fig.legend(
+        handles=legend_handles,
+        labels=legend_labels,
+        loc="upper left",
+        ncol=len(legend_labels),
     )
     plt.show()
 
@@ -60,7 +99,8 @@ def plot_experiment_posterior_vs_ground_truth(
     idata_before_experiment: az.InferenceData,
     idata_after_experiment: az.InferenceData,
     media_param_df: pl.DataFrame,
-    media_feature_name: str,
+    media_feature_names: list[str],
+    figsize: tuple[int, int] = (18, 5),
 ) -> None:
     """Plot the posterior before and after experiment with ground truth.
 
@@ -72,50 +112,88 @@ def plot_experiment_posterior_vs_ground_truth(
         The inference data after experiment.
     media_param_df : pl.DataFrame
         The media parameters dataframe.
-    media_feature_name : str
-        The media feature name to plot against.
+    media_feature_names : list[str]
+        The list of media feature names to plot against.
+    figsize : tuple[int, int], default (18, 5)
+        The size of the Matplotlib's figure
 
     """
-    media_feature_id = int(media_feature_name.split("_")[1])
+    media_feature_ids = [
+        int(name.split("_")[1]) for name in media_feature_names
+    ]
 
-    fig, ax = plt.subplots(1, 3, figsize=(18, 5))
+    fig, ax = plt.subplots(
+        nrows=len(media_feature_names),
+        ncols=3,
+        figsize=figsize,
+    )
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.95, hspace=0.3)
 
-    for i, param in enumerate(["saturation", "shape", "retention_rate"]):
-        ax[i].hist(
-            idata_before_experiment.posterior[f"{param}s"]
-            .values[:, :, media_feature_id]
-            .flatten(),
-            bins=100,
-            density=True,
-            histtype="step",
-            alpha=0.7,
-            label="before experiment",
-        )
-        ax[i].hist(
-            idata_after_experiment.posterior[f"{param}s"]
-            .values[:, :, media_feature_id]
-            .flatten(),
-            bins=100,
-            density=True,
-            histtype="step",
-            alpha=0.7,
-            color="C1",
-            label="after experiment",
-        )
-        ax[i].vlines(
-            media_param_df.filter(
-                pl.col("media_feature") == media_feature_name
-            )[param].item(),
-            ymin=0,
-            ymax=ax[i].get_ylim()[1],
-            color="C2",
-            label="ground truth",
-        )
-        ax[i].set_title(f"{param.capitalize().replace('_', ' ')}")
+    legend_handles, legend_labels = ([], [])
+    for row, (media_feature_id, media_feature_name) in enumerate(
+        zip(media_feature_ids, media_feature_names)
+    ):
+        for col, param in enumerate(["saturation", "shape", "retention_rate"]):
+            h1 = ax[row, col].hist(
+                idata_before_experiment.posterior[f"{param}s"]
+                .values[:, :, media_feature_id]
+                .flatten(),
+                bins=100,
+                density=True,
+                histtype="step",
+                alpha=0.7,
+                label="before experiment",
+            )
+            h2 = ax[row, col].hist(
+                idata_after_experiment.posterior[f"{param}s"]
+                .values[:, :, media_feature_id]
+                .flatten(),
+                bins=100,
+                density=True,
+                histtype="step",
+                alpha=0.7,
+                color="C1",
+                label="after experiment",
+            )
+            h3 = ax[row, col].vlines(
+                media_param_df.filter(
+                    pl.col("media_feature") == media_feature_name
+                )[param].item(),
+                ymin=0,
+                ymax=ax[row, col].get_ylim()[1],
+                color="C2",
+                label="ground truth",
+            )
+            ax[row, col].set_title(f"{param.capitalize().replace('_', ' ')}")
 
-    plt.legend()
-    plt.suptitle(
-        f"Posterior distributions vs Ground truth of "
-        f"{media_feature_name.capitalize().replace('_', ' ')}",
+            if len(legend_handles) == 0:
+                legend_handles = [h1[2][0], h2[2][0], h3]
+                legend_labels = [
+                    "before experiment",
+                    "after experiment",
+                    "ground truth",
+                ]
+
+        # Add the title of each row
+        row_axes = ax[row, :]
+        y_top = max(a.get_position().y1 for a in row_axes)
+        fig.text(
+            x=0.5,
+            y=y_top + 0.1 / len(media_feature_names),
+            s=(
+                f"Posterior distributions vs Ground Truth of "
+                f"Media Channel {media_feature_id + 1}"
+            ),
+            ha="center",
+            va="center",
+            fontsize=15,
+        )
+
+    fig.legend(
+        handles=legend_handles,
+        labels=legend_labels,
+        loc="upper left",
+        ncol=len(legend_labels),
     )
     plt.show()
